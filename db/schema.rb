c69498b31336d7035f5381ab3fb2bcaa83f672df
Davron <github@akil.me>
2014-12-29 00:03:23 -0500

    Add association between post and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229043340) do
+ActiveRecord::Schema.define(version: 20141229045959) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20141229043340) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
