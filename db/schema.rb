b00b62e94a913a350dd36f4e992a0a0d8b3de10a
davronakil <github@akil.me>
2015-03-02 11:31:28 -0500

    Readme update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229045959) do
+ActiveRecord::Schema.define(version: 20150204031618) do
+
+  create_table "cart_items", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
